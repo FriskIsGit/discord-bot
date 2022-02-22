efacb8925436f7ffb60d57d50231cfdf631f5dad
FriskIsGit <frisk00vexor@wp.pl>
2022-02-22 19:52:25 +0100

    added emergency listeners(along with event queue), to fire on multiple events of the same type occurring at roughly the same (channel deletions, kicks, bans)

---
+++ src/main/java/bot/deskort/emergency/EmergencyListener.java
@@ -0,0 +1,158 @@
+package bot.deskort.emergency;
+
+import bot.deskort.Bot;
+import bot.utilities.Actions;
+import bot.utilities.AuditLog;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.audit.ActionType;
+import net.dv8tion.jda.api.audit.AuditLogEntry;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.channel.ChannelDeleteEvent;
+import net.dv8tion.jda.api.events.guild.GuildBanEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+/**
+    Exists for the contingency of a breach
+    Intended to monitor suspicious behavior
+ */
+public class EmergencyListener extends ListenerAdapter{
+
+    public static final int TRIGGER_DELAY_MS = 5000;
+    public static final int EVENT_THRESHOLD = 3;
+
+    private static final HashMap<Long, EventQueue> userIdsToEvents = new HashMap<>(4);
+
+    public volatile boolean awaitKick = false;
+    public volatile HashMap<Guild, AuditLogEntry> guildsToLastKicks = null;
+    public boolean isReady = false;
+
+    public EmergencyListener(JDABuilder jdaBuilder){
+        //for these to work, SERVER MEMBERS INTENT must be enabled in the 'Bot' tab in discord developer panel
+        jdaBuilder.enableIntents(GatewayIntent.GUILD_BANS);
+        jdaBuilder.enableIntents(GatewayIntent.GUILD_MEMBERS);
+        jdaBuilder.setMemberCachePolicy(MemberCachePolicy.ALL);
+    }
+
+    @Override
+    public void onReady(@Nonnull ReadyEvent readyEvent) {
+        isReady = true;
+        List<Guild> connectedGuilds = Bot.getJDAInterface().getGuilds();
+        guildsToLastKicks = new HashMap<>(connectedGuilds.size());
+
+        //to distinguish leave from a kick, the server must have at least one kick entry
+        //this solution accounts for all servers the bot is connected to
+        for(Guild guild : connectedGuilds){
+            List<AuditLogEntry> entryList = AuditLog.retrieveFromAuditLog(ActionType.KICK,1, guild);
+            if(entryList == null || entryList.size() == 0){
+                continue;
+            }
+            guildsToLastKicks.put(guild, entryList.get(0));
+        }
+    }
+
+    @Override
+    public void onGenericEvent(@Nonnull GenericEvent anyEvent) {
+        if(!isReady){
+            return;
+        }
+        //timestamp is made when an event is received
+        //much more accurate and reliable than audit log entry timestamps which tend to be displaced in time by approximately 10 seconds
+        long eventTimestamp = System.currentTimeMillis();
+        if(anyEvent instanceof ChannelDeleteEvent){
+
+            ChannelDeleteEvent channelDeleteEvent = (ChannelDeleteEvent)anyEvent;
+            List<AuditLogEntry> channelDeletions = AuditLog.retrieveFromAuditLog(ActionType.CHANNEL_DELETE,1, channelDeleteEvent.getGuild());
+            AuditLogEntry deletionEntry = channelDeletions.get(0);
+            User userResponsible = deletionEntry.getUser();
+            System.out.println("Channel [" + channelDeleteEvent.getChannel().getName() + "] was deleted by " + Objects.requireNonNull(userResponsible).getName());
+
+            resolveEvent(deletionEntry, eventTimestamp);
+
+        }else if(anyEvent instanceof GuildBanEvent){
+            //ban events subsequently trigger kick events
+            awaitKick = true;
+
+            GuildBanEvent memberBannedEvent = (GuildBanEvent) anyEvent;
+            List<AuditLogEntry> bans = AuditLog.retrieveFromAuditLog(ActionType.BAN,1, memberBannedEvent.getGuild());
+            AuditLogEntry banEntry = bans.get(0);
+            User userResponsible = banEntry.getUser();
+            System.out.println("Member [" + memberBannedEvent.getUser().getName() + "] was banned by " + Objects.requireNonNull(userResponsible).getName());
+
+            resolveEvent(userResponsible, eventTimestamp);
+
+        }else if(anyEvent instanceof GuildMemberRemoveEvent){
+            //will trigger on ban/kick/leave - supposed to differentiate between bans and kicks
+            if(awaitKick) {
+                awaitKick = false;
+                System.out.println("Caught subsequent kick");
+                return;
+            }
+
+            GuildMemberRemoveEvent kickOrLeaveEvent = (GuildMemberRemoveEvent) anyEvent;
+            List<AuditLogEntry> kicks = AuditLog.retrieveFromAuditLog(ActionType.KICK,1, kickOrLeaveEvent.getGuild());
+            AuditLogEntry kickEntry = kicks.get(0);
+            //if true, member left a server (wasn't kicked)
+            AuditLogEntry lastKickEntry = guildsToLastKicks.get(kickOrLeaveEvent.getGuild());
+            if(kickEntry.equals(lastKickEntry)){
+                System.out.println("Member ["
+                        + kickOrLeaveEvent.getUser().getName()
+                        + "] left the server");
+                return;
+            }
+            guildsToLastKicks.put(kickEntry.getGuild(), kickEntry);
+            User userResponsible = kickEntry.getUser();
+            System.out.println("Member ["
+                    + kickOrLeaveEvent.getUser().getName()
+                    + "] was kicked by "
+                    + Objects.requireNonNull(userResponsible).getName());
+
+            resolveEvent(kickEntry, eventTimestamp);
+        }
+    }
+
+    private void resolveEvent(User responsibleUser, long currentTimestamp){
+        long userResponsibleId = responsibleUser.getIdLong();
+        EventQueue eventQueue;
+        if(userIdsToEvents.containsKey(userResponsibleId)){
+            eventQueue = userIdsToEvents.get(userResponsibleId);
+        }
+        else{
+            eventQueue = new EventQueue(EVENT_THRESHOLD);
+            userIdsToEvents.put(userResponsibleId, eventQueue);
+        }
+        eventQueue.append(currentTimestamp);
+        if(eventQueue.isAlarmable(TRIGGER_DELAY_MS)){
+            messageAuthorizedUsers(responsibleUser);
+            System.out.println("---TRIGGERED ALARM---");
+        }
+    }
+    private void resolveEvent(AuditLogEntry auditEntry, long currentTimestamp){
+        User userResponsible = auditEntry.getUser();
+        if(userResponsible == null){
+            return;
+        }
+        resolveEvent(userResponsible, currentTimestamp);
+    }
+
+    private void messageAuthorizedUsers(User responsibleUser){
+        String emergencyMessage = "Emergency triggered"
+                + " by "  + responsibleUser.getName()
+                + " (id: " + responsibleUser.getIdLong() + ")";
+        Actions actions = Bot.getActions();
+        for (long id : Bot.AUTHORIZED_USERS){
+            actions.messageUser(id, emergencyMessage);
+        }
+        
+    }
+}
---
+++ src/main/java/bot/deskort/emergency/EmergencyTest.java
@@ -0,0 +1,77 @@
+package bot.deskort.emergency;
+
+import bot.deskort.Bot;
+import bot.utilities.Channels;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class EmergencyTest{
+    private static JDA jdaInterface;
+    private static Guild guild;
+    private static Channels channels;
+    public static long guildId = 519857141494841344L;
+
+    public static String[] channelNames = {
+            "test_channel_1",
+            "test_channel_2",
+            "test_channel_3",
+            "test_channel_4",
+            "test_channel_5"
+    };
+    public static List<TextChannel> textChannels = new ArrayList<>(channelNames.length);
+    public static void initAndCreateChannels(){
+        jdaInterface = Bot.getJDAInterface();
+        guild = jdaInterface.getGuildById(guildId);
+        channels = Bot.getChannels();
+
+        for(String channelName : channelNames){
+            Objects.requireNonNull(guild).createTextChannel(channelName).queue();
+        }
+
+    }
+    public static void getAndDeleteChannels(){
+        channels = Bot.getChannels();
+
+        for(String channelName : channelNames){
+            TextChannel channel = channels.getTextChannel(channelName);
+            if(channel != null){
+                textChannels.add(channel);
+            }
+        }
+        for(TextChannel textChannel : textChannels){
+            textChannel.delete().queue();
+        }
+
+    }
+    public static void banAndKickSimultaneously(long id1, long id2, long id3){
+        jdaInterface = Bot.getJDAInterface();
+        guild = jdaInterface.getGuildById(guildId);
+
+        User user1 = jdaInterface.retrieveUserById(id1).complete();
+        User user2 = jdaInterface.retrieveUserById(id2).complete();
+        User user3 = jdaInterface.retrieveUserById(id3).complete();
+        if(user1 != null && user2 != null && user3 != null){
+            new Thread(() -> {
+                guild.ban(user1.getId(), 0, "Test ban1").complete();
+            }).start();
+            try{
+                Thread.sleep(250);
+            }catch (InterruptedException interruptedException){
+                interruptedException.printStackTrace();
+            }
+            new Thread(() -> {
+                guild.kick(user2.getId(), "Test kick2").complete();
+            }).start();
+            new Thread(() -> {
+                guild.ban(user3.getId(), 0, "Test ban3").complete();
+            }).start();
+        }
+
+    }
+}
