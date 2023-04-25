681664e28c58d4ee9ecc12fba6ad896a169dec72
FriskIsGit <friskisgit@users.noreply.github.com>
2023-04-25 21:34:35 +0200

    removed useless code

--- src/main/java/bot/music/AudioConverter.java
+++ src/main/java/bot/music/AudioConverter.java
@@ -15,10 +15,6 @@ import java.io.IOException;
 
 public class AudioConverter{
     //ffmpeg -i inputFile.m4a -codec copy converted.m4a
-    public final static byte[] FTYP = {102, 116, 121, 112};
-    public final static byte[] ISO = {105, 115, 111};
-    public final static byte[] MOOV = {109, 111, 111, 118};
-    public final static byte[] MDAT = {109, 100, 97, 116};
     public final static short WAVE_OFFSET = 44;
     public final static float SAMPLE_RATE_OFFSET = 500f;
     public final static float TARGET_SAMPLE_RATE = 48000f;
@@ -118,67 +114,7 @@ public class AudioConverter{
         System.arraycopy(songBytes, WAVE_OFFSET, rawAudio, 0, rawAudio.length);
         return rawAudio;
     }
-    public static String detectFtyp(byte[] songBytes) throws IndexOutOfBoundsException{
-        for (int i = 0, len = songBytes.length; i < len; i++){
-            if(songBytes[i] == FTYP[0]){
-                boolean matching = true;
-                for (int j = 1; j < 4; j++){
-                    if(songBytes[i+j] != FTYP[j]){
-                        matching = false;
-                        break;
-                    }
-                }
-                if(matching){
-                    String str = "";
-                    i+=4;
-                    for (int k = 0; k < 4; k++){
-                        str+= (char)songBytes[i+k];
-                    }
-                    return str;
-                }
-            }
-        }
-        return "";
-    }
-    public static void replaceFtyp(byte[] songBytes, String newFtyp) {
-        int newFtypLen = newFtyp.length();
-        byte[] ftyp;
-        if(newFtypLen == 3){
-            ftyp = new byte[]{(byte) newFtyp.charAt(0),(byte) newFtyp.charAt(1),(byte) newFtyp.charAt(2), 0};
-        }else if(newFtypLen == 4){
-            ftyp = new byte[]{(byte) newFtyp.charAt(0),(byte) newFtyp.charAt(1),(byte) newFtyp.charAt(2),(byte) newFtyp.charAt(3)};
-        }else{
-            return;
-        }
 
-        for (int i = 0, len = songBytes.length-4; i < len; i++){
-            if (songBytes[i] == FTYP[0] && songBytes[i+1] == FTYP[1] && songBytes[i+2] == FTYP[2] && songBytes[i+3] == FTYP[3]){
-                i+=4;
-                for (int k = 0; k < 4; k++){
-                    songBytes[i+k] = ftyp[k];
-                }
-                break;
-            }
-        }
-    }
-    public static void replaceIso(byte[] songBytes, String newEntireIso) {
-        int newIsoLen = newEntireIso.length();
-        if(newIsoLen<4 || newIsoLen>8){
-            return;
-        }
-        byte[] newIso = new byte[newIsoLen];
-        for (int i = 0; i < newIsoLen; i++){
-            newIso[i] = (byte) newEntireIso.charAt(i);
-        }
-        for (int i = 0, len = songBytes.length - newIsoLen + 3; i < len; i++){
-            if (songBytes[i] == ISO[0] && songBytes[i+1] == ISO[1] && songBytes[i+2] == ISO[2]){
-                for (int k = 0; k < newIsoLen; k++){
-                    songBytes[i+k] = newIso[k];
-                }
-                break;
-            }
-        }
-    }
     public static javax.sound.sampled.AudioFormat jcodecFormatToPureJavaFormat(org.jcodec.common.AudioFormat givenFormat){
         if(givenFormat == null){
             System.err.println("Given jcodec format is null");
@@ -191,38 +127,4 @@ public class AudioConverter{
                 givenFormat.isSigned(),
                 givenFormat.isBigEndian());
     }
-
-    public static void convertDashBufferToMPEG(byte[] buffer){
-        for (int i = 0, bufLen = buffer.length - 3; i < bufLen; i++){
-            int begin;
-            if (buffer[i] == MOOV[0] && buffer[i+1] == MOOV[1] && buffer[i+2] == MOOV[2] && buffer[i+3] == MOOV[3]){
-                begin = i;
-                for (; i < bufLen; i++){
-                    //end of slice
-                    if(buffer[i] == MDAT[0] && buffer[i+1] == MDAT[1] && buffer[i+2] == MDAT[2] && buffer[i+3] == MDAT[3]){
-                        moveRangeToEnd(buffer, begin, i);
-                        return;
-                    }
-                }
-            }
-        }
-    }
-    //exclusive index
-    private static void moveRangeToEnd(byte[] buf, int beg, int end){
-        if(beg >= end){
-            return;
-        }
-        int sliceLen = end-beg;
-        byte[] slice = new byte[sliceLen];
-        System.arraycopy(buf,beg,slice,0,sliceLen);
-        //copy back
-        for (int i = 0; i < sliceLen; i++){
-            buf[i + beg] = buf[i + end];
-        }
-        //insert slice
-        for (int i = buf.length-sliceLen, s = 0; s < sliceLen; i++, s++){
-            buf[i] = slice[s];
-        }
-    }
-
 }
